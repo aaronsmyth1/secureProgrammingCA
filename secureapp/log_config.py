2323fda9969ce03eb76baf19927b7d3e269a231a
Aaron Smyth <117903652+aaronsmyth1@users.noreply.github.com>
2025-12-01 16:18:44 +0000

    Delete secureapp directory

--- secureapp/log_config.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-def setupLogging():
-    logging.basicConfig(
-        filename='app.log',
-        level=logging.INFO,  
-        format='%(asctime)s %(levelname)s:%(message)s'
-        )
